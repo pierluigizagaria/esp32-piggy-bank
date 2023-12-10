ffa8a93d68d1f636819e7e17d4916b53f4c2a7a3
Pierluigi Zagaria <pierluigizagaria@gmail.com>
2023-12-10 19:49:05 +0100

    Added coin detection and reworked sound managment

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,17 @@
 #include <ESP32Servo.h>
 #include <TickTwo.h>
 #include <sqlite3.h>
-#include "sounds.h"
 
-#define SOUND_FOLDER_PATH "/sound"
+#define COIN_SOUND_FOLDER_PATH "/sound/coin"
 #define DATABASE_PATH "/sd/database.db"
+#define MAX_FILES 32
 
-AudioSourceSD source(SOUND_FOLDER_PATH, "mp3", PIN_AUDIO_KIT_SD_CARD_CS);
+AudioSourceSD source(COIN_SOUND_FOLDER_PATH, "mp3", PIN_AUDIO_KIT_SD_CARD_CS);
 AudioKitStream kit;
 MP3DecoderHelix decoder;
 AudioPlayer player(source, kit, decoder);
+
+byte coinPulses = 0;
 
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 4, 1);
@@ -32,12 +34,20 @@ uint16_t timeInput, timeLabel;
 sqlite3 *db;
 sqlite3_stmt *res;
 
-void playSound(const String sound);
+void IRAM_ATTR coinInterrupt();
+void coinDetected(int coin);
+void loadFiles(const char *path, char *files[], int &count);
+void playCoinSound(int coin);
 void getTimeCallback(Control *sender, int type);
 void syncTimeCallback(Control *sender, int type);
 void updateTimeLabel();
 
 TickTwo timeLabelUpdater(updateTimeLabel, 1000, 0, MILLIS);
+
+TickTwo pulsesWatchdog([]
+                       {coinDetected(coinPulses); 
+                        coinPulses = 0; },
+                       150, 1, MILLIS);
 
 void setup()
 {
@@ -49,14 +59,16 @@ void setup()
   cfg.sd_active = true;
   kit.begin(cfg);
 
+  AudioActions &actions = kit.audioActions();
+  actions.setEnabled(PIN_KEY6, false);
+
+  pinMode(PIN_KEY6, INPUT_PULLUP);
+  attachInterrupt(PIN_KEY6, coinInterrupt, RISING);
+
   SD.begin(PIN_AUDIO_KIT_SD_CARD_CS, AUDIOKIT_SD_SPI);
 
+  kit.setVolume(90);
   player.setVolume(1.0);
-
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(apIP, apIP, subnet);
-  WiFi.softAP("PIGGY BANK");
-  dnsServer.start(DNS_PORT, "*", apIP);
 
   Wire.setPins(22, 21);
   if (!rtc.begin())
@@ -68,6 +80,11 @@ void setup()
     espRTC.setTime(rtc.now().unixtime());
   }
 
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, subnet);
+  WiFi.softAP("PIGGY BANK");
+  dnsServer.start(DNS_PORT, "*", apIP);
+
   sqlite3_initialize();
   if (sqlite3_open(DATABASE_PATH, &db) != SQLITE_OK)
   {
@@ -77,12 +94,12 @@ void setup()
   timeLabel = ESPUI.label("Date & Time", ControlColor::Sunflower, "");
   timeInput = ESPUI.addControl(Time, "", "", None, 0, getTimeCallback);
   ESPUI.addControl(Button, "Update time", "Sync with device", None, timeLabel, syncTimeCallback);
-
   ESPUI.begin("PIGGY BANK");
 
   timeLabelUpdater.start();
+  srand(espRTC.getLocalEpoch());
 
-  playSound(sounds[HORN]);
+  playCoinSound(1);
 }
 
 void loop()
@@ -91,12 +108,62 @@ void loop()
   kit.processActions();
   player.copy();
   timeLabelUpdater.update();
+  pulsesWatchdog.update();
 }
 
-void playSound(const String sound)
+void IRAM_ATTR coinInterrupt()
 {
-  String path = String(SOUND_FOLDER_PATH) + "/" + sound;
-  player.setPath(path.c_str());
+  coinPulses++;
+  pulsesWatchdog.start();
+}
+
+void coinDetected(int coin)
+{
+  Serial.print("Detected coin: ");
+  Serial.println(coin);
+  playCoinSound(coin);
+}
+
+void loadFiles(const char *path, char *files[], int &count)
+{
+  File directory = SD.open(path);
+
+  if (!directory.isDirectory())
+  {
+    Serial.printf("Path %s does not exist\n", path);
+    return;
+  }
+
+  count = 0;
+
+  while (File entry = directory.openNextFile())
+  {
+    if (!entry.isDirectory())
+    {
+      if (count < MAX_FILES)
+      {
+        files[count] = strdup(entry.path());
+        count++;
+      }
+    }
+    entry.close();
+  }
+  directory.close();
+}
+
+void playCoinSound(int coin)
+{
+  char path[256];
+  sprintf(path, "%s/%d", COIN_SOUND_FOLDER_PATH, coin);
+
+  char *files[MAX_FILES];
+  int count = 0;
+  loadFiles(path, files, count);
+
+  for (int i = 0; i < count; i++)
+    Serial.printf("%d. %s\n", i, files[i]);
+
+  player.setPath(files[rand() % count]);
   player.play();
 }
 
